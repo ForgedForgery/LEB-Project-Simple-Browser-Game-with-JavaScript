80d3df95b7eaa8871f9b4add82eabdc41c7e16ae
MaximilianSchielke <33453349+MaximilianSchielke@users.noreply.github.com>
2017-11-23 09:12:06 +0100

    Update game.js

--- Titel/game.js
+++ Titel/game.js
@@ -1,9 +1,9 @@
 function titleUpdate(){
-    c.fillStyle =  "black";
+    c.fillStyle = "black";
     c.strokeStyle = "blue";
     c.font = "20px Comic Sans MS";
     c.textAlign = 'center';
-    c.textBaseline = 'middle';
+  //  c.textBaseline = 'middle';
     c.fillText('Resource Collector',400,100);
     
     /*var Button = function(config) 
@@ -13,37 +13,39 @@ function titleUpdate(){
         this.width = config.width || 200;
         this.height = config.height || 75;
     };*/
-
-    var Button = function(config) {
-        this.x = config.x || 0;
-        this.y = config.y || 0;
-        this.width = config.width || 150;
-        this.height = config.height || 50;
-        this.label = config.label || "Click";
-        this.onClick = config.onClick || function() {};
-    };
-
-    Button.prototype.draw = function() {
-        c.fillStyle(0, 234, 255);
-        c.rect(this.x, this.y, this.width, this.height, 5);
-        c.fillStyle(0, 0, 0);
-        c.textSize(19);
-        c.textAlign='middle';
-        c.fillText(this.label, this.x+10, this.y+this.height/4);
-    };
-
-    Button.prototype.isMouseInside = function() {
-        return mouseX > this.x &&
-               mouseX < (this.x + this.width) &&
-               mouseY > this.y &&
-               mouseY < (this.y + this.height);
-    };
-
-    Button.prototype.handleMouseClick = function() {
-        if (this.isMouseInside()) {
-            this.onClick();
+    
+    class Button {
+        constructor(config) {
+            this.x = config.x || 0;
+            this.y = config.y || 0;
+            this.width = config.width || 150;
+            this.height = config.height || 50;
+            this.label = config.label || "Click";
+            this.onClick = config.onClick || function() {};
         }
-    };
+        
+        draw() {
+            c.fillStyle = "black";
+            c.rect(this.x, this.y, this.width, this.height, 5);
+            c.fillStyle = "blue";
+            c.textSize = "19";
+            c.textAlign ='middle';
+            c.fillText(this.label, this.x+10, this.y+this.height/4);
+        }    
+        
+        isMouseInside() {
+            return mouseX > this.x &&
+                mouseX < (this.x + this.width) &&
+                mouseY > this.y &&
+                mouseY < (this.y + this.height);
+        }   
+
+        handelMouseClick {
+            if (this.isMouseInside()) {
+                this.onClick();
+            }
+        }
+    }
 
     var btn1 = new Button({
         x: 300,
@@ -73,7 +75,7 @@ function titleUpdate(){
         btn1.handleMouseClick();
         btn2.handleMouseClick();
     };    
-};
+}
 
 
 
