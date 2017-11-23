e07788e6dad07efbbc208734b879d7f6707ac2ba
MaximilianSchielke <33453349+MaximilianSchielke@users.noreply.github.com>
2017-11-23 10:10:24 +0100

    Add files via upload

--- Titel/game.js
+++ Titel/game.js
@@ -1,51 +1,12 @@
 function titleUpdate(){
+    c.beginPath();
     c.fillStyle = "black";
     c.strokeStyle = "blue";
-    c.font = "20px Comic Sans MS";
+    c.font = "40px Comic Sans MS";
     c.textAlign = 'center';
-  //  c.textBaseline = 'middle';
+    c.textBaseline = 'middle';
     c.fillText('Resource Collector',400,100);
-    
-    /*var Button = function(config) 
-    {
-       this.x = config.x || 0;
-        this.y = config.y || 0;
-        this.width = config.width || 200;
-        this.height = config.height || 75;
-    };*/
-    
-    class Button {
-        constructor(config) {
-            this.x = config.x || 0;
-            this.y = config.y || 0;
-            this.width = config.width || 150;
-            this.height = config.height || 50;
-            this.label = config.label || "Click";
-            this.onClick = config.onClick || function() {};
-        }
-        
-        draw() {
-            c.fillStyle = "black";
-            c.rect(this.x, this.y, this.width, this.height, 5);
-            c.fillStyle = "blue";
-            c.textSize = "19";
-            c.textAlign ='middle';
-            c.fillText(this.label, this.x+10, this.y+this.height/4);
-        }    
-        
-        isMouseInside() {
-            return mouseX > this.x &&
-                mouseX < (this.x + this.width) &&
-                mouseY > this.y &&
-                mouseY < (this.y + this.height);
-        }   
-
-        handelMouseClick {
-            if (this.isMouseInside()) {
-                this.onClick();
-            }
-        }
-    }
+    c.closePath();   
 
     var btn1 = new Button({
         x: 300,
