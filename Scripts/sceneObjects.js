4c0631f0914266542b41196ef72df70a4a3759e5
ForgedForgery <hiandreas@googlemail.com>
2017-12-14 15:09:37 +0100

    Update sceneObjects.js

--- Scripts/sceneObjects.js
+++ Scripts/sceneObjects.js
@@ -31,34 +31,6 @@ class SceneManager {
     }
 }
 
-class MainGame {
-    constructor(inPlayerReference, inCollectiblesReference) {
-        this.player = inPlayerReference;
-        this.collectibles = inCollectiblesReference;
-        this.gameUI = new GameUI(this.player);
-    }
-    
-    update() {
-        this.collectibles.update();
-        this.player.update();
-        this.player.checkCollisionWith(this.collectibles);
-        this.gameUI.update();
-    }
-    
-    draw() {
-        this.drawBG();
-        this.collectibles.draw();
-        this.player.draw();
-        this.gameUI.draw();
-    }
-    
-    drawBG() {
-        canvasContext.fillStyle = gameBGColor;
-        canvasContext.rect(0, 0, width, height + heightUI);
-        canvasContext.fill();
-    }
-}
-
 class TitleMenu {
     constructor(inPlayerReference) {
         this.player = inPlayerReference;
@@ -98,6 +70,113 @@ class TitleMenu {
         canvasContext.textAlign = 'center';
         canvasContext.textBaseline = 'middle';
         canvasContext.fillText('Resource Collector',400,100);
+        canvasContext.closePath();
+    }
+}
+
+class MainGame {
+    constructor(inPlayerReference, inCollectiblesReference) {
+        this.player = inPlayerReference;
+        this.collectibles = inCollectiblesReference;
+        this.gameUI = new GameUI(this.player);
+    }
+    
+    update() {
+        this.collectibles.update();
+        this.player.update();
+        this.player.checkCollisionWith(this.collectibles);
+        this.gameUI.update();
+    }
+    
+    draw() {
+        this.drawBG();
+        this.collectibles.draw();
+        this.player.draw();
+        this.gameUI.draw();
+    }
+    
+    drawBG() {
+        canvasContext.fillStyle = gameBGColor;
+        canvasContext.rect(0, 0, width, height + heightUI);
+        canvasContext.fill();
+    }
+}
+
+class GameUI {
+    constructor(inPlayerReference) {
+        this.x = 0;
+        this.y = height;
+        this.width = width;
+        this.height = heightUI;
+        
+        this.player = inPlayerReference;
+        
+        this.menuButton = new Button({
+            
+        });
+        
+        this.saveButton = new Button({
+            x: this.x + 627,
+            y: this.y + 25,
+            width: 100,
+            label: "Save",
+            fontSize: "20px",
+            onClick: () => doSave()
+        });
+        
+        this.shopButton = new Button({
+            x: this.x + 740,
+            y: this.y + 25,
+            width: 100,
+            label: "Shop",
+            fontSize: "20px",
+           //onClick effekt für Szenenwechseln
+        });
+        
+        this.playerNameField = new TextField({
+            x: this.x + 10,
+            y: this.y + 12.5,
+            text: this.player.name,
+            align: 'left'
+        });
+                
+        this.playerScorePreField = new TextField({
+            x: this.x + 10,
+            y: this.y + 37.5,
+            text: "Score: ",
+            align: 'left'
+        });
+        
+        this.playerScoreField = new TextField({
+            x: this.x + 75,
+            y: this.y + 37.5,
+            color: playerScoreFieldColor,
+            text: this.player.score,
+            align: 'start'
+        });
+    }
+    
+    update() {
+        this.saveButton.update();
+        this.shopButton.update();
+        this.playerNameField.setTextTo(this.player.name);
+        this.playerScoreField.setTextTo(this.player.score);
+    }
+    
+    draw() {
+        this.drawBackground();
+        this.playerNameField.draw();
+        this.saveButton.draw();
+        this.shopButton.draw();
+        this.playerScoreField.draw();
+        this.playerScorePreField.draw();
+    }
+    
+    drawBackground() {
+        canvasContext.beginPath();
+        canvasContext.rect(this.x, this.y, this.width, this.height);
+        canvasContext.fillStyle = gameUIColor;
+        canvasContext.fill();
         canvasContext.closePath();
     }
 }
